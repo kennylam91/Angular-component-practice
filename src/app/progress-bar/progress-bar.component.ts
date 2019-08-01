22ea7b1649e6c7b8696c7d04e365fe62052cceb8
kennylam91 <phamvanlam1991@gmail.com>
2019-08-01 17:37:57 +0700

    refactor progress-bar

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -9,7 +9,7 @@ export class ProgressBarComponent implements OnInit {
 
   @Input() backgroundColor = '#d9d9d9';
   @Input() progressColor = '#4CAF50';
-  @Input() width = '50';
+  @Input() width = '50%';
 
   constructor() {
   }
