8effed071d11f2d0a5dace90d1d90b14f3ad1213
kennylam91 <phamvanlam1991@gmail.com>
2019-08-01 17:36:20 +0700

    done card-name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { NameCardComponent } from './name-card/name-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountdownTimerComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    NameCardComponent
   ],
   imports: [
     BrowserModule
